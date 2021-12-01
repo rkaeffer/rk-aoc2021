988f230d3c7ffd2d0ac90ebaeb837f44116a1584
Rémi Kaeffer <remi.kaeffer@soprasteria.com>
2021-12-01 13:42:39 +0100

    Day 1 Chall 2

--- java/src/main/java/fr/rk/aoc/challenge/Day1.java
+++ java/src/main/java/fr/rk/aoc/challenge/Day1.java
@@ -20,4 +20,18 @@ public final class Day1 {
 
     }
 
+    /**
+     * Return the number of times a values increased comparing to previous one for a given list of Integer
+     *
+     * @param values input values
+     * @return number of times a values increased comparing to previous one
+     */
+    public static long nbTimesSumValuesIncreased(List<Long> values) {
+        return IntStream.range(0, values.size()-3)
+                .reduce(0, (partial, current) ->
+                        partial + ( (values.get(current)+values.get(current+1) + values.get(current+2)) < (values.get(current+1) + values.get(current+2) + values.get(current+3)) ? 1 : 0));
+
+    }
+
+
 }
--- java/src/test/java/fr/rk/aoc/challenge/Day1Test.java
+++ java/src/test/java/fr/rk/aoc/challenge/Day1Test.java
@@ -18,13 +18,29 @@ public class Day1Test {
     @Test
     public void testNbTimesValuesIncreased() {
         ArrayList<Long> inputTest = new ArrayList<>(Arrays.asList(199L,200L,208L,210L,200L,207L,240L,269L,260L,263L));
-        MatcherAssert.assertThat("Numbers of times wher values increased in list is 7", Day1.nbTimesValuesIncreased(inputTest), Matchers.equalTo(7L));
+        MatcherAssert.assertThat("Numbers of times where values increased in list is 7", Day1.nbTimesValuesIncreased(inputTest), Matchers.equalTo(7L));
     }
+
+    @Test
+    public void testNbTimesSumValuesIncreased() {
+        ArrayList<Long> inputTest = new ArrayList<>(Arrays.asList(199L,200L,208L,210L,200L,207L,240L,269L,260L,263L));
+        MatcherAssert.assertThat("Numbers of times where sum values increased in list is 5", Day1.nbTimesSumValuesIncreased(inputTest), Matchers.equalTo(5L));
+    }
+
 
     @Test
     public void getFirstChallengeResult() {
         try (Stream<String> lines = Files.lines(Paths.get("src/test/resources/day1/firstChall/input.txt"))) {
             log.info(String.valueOf(Day1.nbTimesValuesIncreased(lines.map(Long::valueOf).collect(Collectors.toList()))));
+        } catch (IOException ioException) {
+            log.error("Unable to read input file : {}", ioException.getMessage());
+        }
+    }
+
+    @Test
+    public void getSecondChallengeResult() {
+        try (Stream<String> lines = Files.lines(Paths.get("src/test/resources/day1/firstChall/input.txt"))) {
+            log.info(String.valueOf(Day1.nbTimesSumValuesIncreased(lines.map(Long::valueOf).collect(Collectors.toList()))));
         } catch (IOException ioException) {
             log.error("Unable to read input file : {}", ioException.getMessage());
         }
